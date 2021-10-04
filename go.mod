136124e66fbf8c45e2a0649dfa735c1bb030fa18
FabioSebs <sebsphotosofficial@gmail.com>
2021-10-03 20:01:11 -0400

    Added Bar Charts

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/FabioSebs/Data-Visualization-Golang
+
+go 1.13
+
+require github.com/go-echarts/go-echarts/v2 v2.2.4
--- main.go
+++ main.go
@@ -6,13 +6,54 @@ import (
 	"io"
 	"log"
 	"os"
+	"sort"
+
+	"github.com/go-echarts/go-echarts/v2/charts"
+	"github.com/go-echarts/go-echarts/v2/opts"
 )
 
+type Pair struct {
+	Key   string
+	Value int
+}
+
+type PairList []Pair
+
+func (p PairList) Len() int               { return len(p) }
+func (p PairList) Swap(i, j int)          { p[i], p[j] = p[j], p[i] }
+func (p PairList) Less(i, j int) bool     { return p[i].Value < p[j].Value }
+func (p PairList) ReturnVal(i int) string { return p[i].Key }
+
+func generateBarItems(data PairList) []opts.BarData {
+	barData := make([]int, 7)
+	items := make([]opts.BarData, 0)
+	for k, _ := range data {
+		if len(barData) == 7 {
+			break
+		} else {
+			barData = append(barData, data[k].Value)
+		}
+	}
+	for _, v := range barData {
+		items = append(items, opts.BarData{Value: v})
+	}
+	return items
+}
+
 func main() {
-	// Reading the CSV and Extracting the Data
+	//Variables
 	f, err := os.Open("brooklyn.csv")
 	data := []string{}
-	freq := make(map[string]int)
+	freq := map[string]int{}
+	bar := charts.NewBar()
+	bar.SetGlobalOptions(charts.WithTitleOpts(opts.Title{
+		Title:    "Common Foods Wasted in Brooklyn",
+		Subtitle: "Edible and wasted food found in retailer trash piles around Brooklyn",
+	}))
+	p := make(PairList, 85)
+	i := 0
+
+	// Reading the CSV and Extracting the Data
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -31,7 +72,7 @@ func main() {
 	}
 	// fmt.Println(data)
 
-	//Counts of Elements in the Data
+	//Count of Elements in the Data
 	for _, item := range data {
 		_, exist := freq[item]
 
@@ -42,7 +83,33 @@ func main() {
 		}
 	}
 
+	// for k, v := range freq {
+	// 	fmt.Printf("Item : %s \nCount: %d\n", k, v)
+	// }
+
+	//Sorting Map in Descending Order
 	for k, v := range freq {
-		fmt.Printf("Item : %s \nCount: %d\n", k, v)
+		p[i] = Pair{k, v}
+		i++
 	}
+
+	sort.Sort(sort.Reverse(p))
+
+	for _, k := range p {
+		fmt.Printf("%v\t%v\n", k.Key, k.Value)
+	}
+
+	//Setting Instance of Bar
+	bar.SetXAxis([]string{
+		p.ReturnVal(0),
+		p.ReturnVal(1),
+		p.ReturnVal(2),
+		p.ReturnVal(3),
+		p.ReturnVal(4),
+		p.ReturnVal(5),
+		p.ReturnVal(6),
+	})
+
+	e, _ := os.Create("brooklyn-data.html")
+	bar.Render(e)
 }
